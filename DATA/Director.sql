b82f5ab15eb7de9079eae4bd579bcf7dea436dc8
WaliDiabate <wali.diabate3@etu.univ-lorraine.fr>
2026-04-09 21:43:45 +0200

    Delete DATA directory

--- DATA/Director.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Film_Direction (
-    film_id INTEGER,
-    director_id INTEGER,
-    role VARCHAR(50) DEFAULT 'Director', -- Optionnel : permet de distinguer Co-Director / Lead Director
-    PRIMARY KEY (film_id, director_id), -- Clé primaire composée : un duo film-réalisateur est unique
-    FOREIGN KEY (film_id) REFERENCES Film(film_id),
-    FOREIGN KEY (director_id) REFERENCES Director(director_id)
-);
